0dea4c08f0c940d40018b2cc4871c8ad2e467dad
sneachda <savela.aleks@gmail.com>
2020-08-17 12:39:35 +0100

    comments added to app.py

--- app.py
+++ app.py
@@ -43,14 +43,13 @@ Recipes
 @app.route('/get_recipes')
 def get_recipes():
     # pagination - learnt through youtube video from Pretty Printed
-    # modified from the Shane Muirhead's project
+    # modified from the Shane Muirhead's project, inspired by irinatu17 project
     limit_per_page = 3
     current_page = int(request.args.get('current_page', 1))
     number_of_all_rec = mongo.db.recipes.count()
     pages = range(1, int(math.ceil(number_of_all_rec / limit_per_page)) + 1)
     recipes = mongo.db.recipes.find().sort('_id', pymongo.ASCENDING).skip(
         (current_page - 1) * limit_per_page).limit(limit_per_page)
-    # get total of all the recipes in db
     return render_template("recipes.html", recipes=recipes,
                            title='All Recipes', current_page=current_page,
                            pages=pages, number_of_all_rec=number_of_all_rec)
@@ -86,6 +85,7 @@ def get_juices():
 
 @app.route('/recipe_full_page/<recipe_id>')
 def recipe_full_page(recipe_id):
+    # searches DB by ID
     full_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     return render_template("recipe_full_page.html",
                            full_recipe=full_recipe,
@@ -94,12 +94,16 @@ def recipe_full_page(recipe_id):
 
 '''
 My Account
+Recipes Routes - CRUD function:
+Create, Read, Update, Delete
 '''
 
 
 # My Recipes
+# READ
 @app.route('/my_recipes/')
 def my_recipes():
+    # to view all recipes created by username
     if 'username' in session:
         limit_per_page = 3
         current_page = int(request.args.get('current_page', 1))
@@ -111,7 +115,8 @@ def my_recipes():
                                recipes=recipes, current_page=current_page, number_of_my_rec=number_of_my_rec)
 
 
-# add recipe
+# add recipe by logged in user
+# CREATE
 @app.route('/add_recipe')
 def add_recipe():
     if 'username' in session:
@@ -125,9 +130,10 @@ def add_recipe():
         return render_template("login.html")
 
 
-# insert recipe
+# inserts recipe created to 'recipes' collection in DB
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
+    #  inserts new recipe after submission the form
     if request.method == 'POST':
         recipes = mongo.db.recipes
         recipes.insert_one({
@@ -145,6 +151,8 @@ def insert_recipe():
     return redirect(url_for('my_recipes'))
 
 
+# UPDATE
+# provides user with a form to edit recipe with pre-populated fields.
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     if 'username' in session:
@@ -158,6 +166,7 @@ def edit_recipe(recipe_id):
 
 @app.route('/update_recipe/<recipe_id>', methods=['POST'])
 def update_recipe(recipe_id):
+    # Updates the selected recipe after submission
     if request.method == 'POST':
         recipes = mongo.db.recipes
         recipes.update({"_id": ObjectId(recipe_id)}, {
@@ -174,6 +183,8 @@ def update_recipe(recipe_id):
         return redirect(url_for("recipe_full_page", recipe_id=recipe_id))
 
 
+# DELETE
+# removes selected recipe from DB
 @app.route('/delete_recipe/<recipe_id>')
 def delete_recipe(recipe_id):
     mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
@@ -182,7 +193,7 @@ def delete_recipe(recipe_id):
 
 
 '''
-User 
+User: Register, LogIn, LogOut
 '''
 
 
@@ -250,6 +261,7 @@ def logout():
         return redirect(url_for('index'))
 
 
+# cookies setting recommended by Chrome
 def set_cookie(response, *args, **kwargs):
     cookie = dump_cookie(*args, **kwargs)
 
