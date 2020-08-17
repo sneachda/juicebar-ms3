5358f51353dfeba7dbf6d2f58cc0c0fee77c69ea
sneachda <savela.aleks@gmail.com>
2020-08-17 07:16:53 +0100

    updated footer style

--- app.py
+++ app.py
@@ -249,21 +249,6 @@ def logout():
         return redirect(url_for('index'))
 
 
-'''
-Error
-'''
-
-
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
-
-
 if __name__ == '__main__':
     app.run(host=os.getenv("IP", "0.0.0.0"),
             port=int(os.getenv("PORT", "5000")),
